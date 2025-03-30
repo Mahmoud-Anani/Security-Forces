11b5a00140357109ddb346094c6ff1c093e294d9
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2025-03-30 16:55:28 +0200

    fix: correct spelling of "البنية التحتية" in meta descriptions across multiple components

--- src/routes/dataBaseView.tsx
+++ src/routes/dataBaseView.tsx
@@ -3,14 +3,13 @@ import ExcelReader from "../components/excel/ExcelReader";
 import BtnsPrint from "../components/dbView/btnsPrint";
 
 function DataBaseView() {
-  
   return (
     <div className={`container mx-auto sm:px-0 px-2 my-5`}>
       <Helmet>
         <title>شؤون المجندين || عرض البيانات</title>
         <meta
           name="description"
-          content="تطبيق شؤون المجندين لوزارة الداخلية يهدف الي تطوير البنية التحتيتة لوزارة الداخلية وزيادة كفاءة الخدمات المقدمة للشعب المصري وتسهيل الاجراءات الخاصة بالمجندين"
+          content="تطبيق شؤون المجندين لوزارة الداخلية يهدف الي تطوير البنية التحتية لوزارة الداخلية وزيادة كفاءة الخدمات المقدمة للشعب المصري وتسهيل الاجراءات الخاصة بالمجندين"
         />
         <link rel="icon" href="/favicon.ico" />
       </Helmet>
