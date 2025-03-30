b522aab20fee4bafd05abbbd66e8e5753106a4c8
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2025-03-30 11:22:57 +0200

    feat: update PWA configuration with new manifest details and include workbox-window; clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Ajwaa | أجواء",
+    short_name: "Ajwaa",
+    description:
+      "أجواء لحجز رحلات طيران و فنادق و سيارات باسهل الطرق الممكنة مع اقل التكاليف",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "225x225",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    VitePWA({
-      workbox: {
-        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // Set limit to 4 MiB
-      },
-      registerType: "autoUpdate",
-      manifest: {
-        name: "My PWA App",
-        short_name: "PWA App",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [tailwindcss(), react(), VitePWA(manifestForPlugin)],
 });
