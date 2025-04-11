06135ba7aaadc7edd36a6567888859af880315ff
Mahmoud-Anani <mahmoud18957321@gmail.com>
2025-04-11 15:14:05 +0200

    handle print in mobile

--- src/components/dbView/printDataOfSolder.tsx
+++ src/components/dbView/printDataOfSolder.tsx
@@ -148,12 +148,12 @@ function PrintDataOfSolder({
             <div
               ref={contentRef}
               key={dataIndex}
-              className={`sm:text-2xl text-4xl flex flex-col gap-4 relative left-[-3%] w-[740px] h-[1095px] z-10`}
+              className={`sm:text-2xl text-6xl flex flex-col gap-4 relative left-[-3%] w-[740px] h-[1095px] z-10`}
             >
               <div className={`absolute z-10 top-[23.8%]`}>
                 <p style={{direction:"rtl"}} className={`px-8 font-semibold text-black`}>{value}</p>
                 <div
-                  className={`text-center flex flex-col gap-[20px] mr-12 sticky sm:mt-[65px] mt-[150px] font-semibold text-black`}
+                  className={`text-center flex flex-col gap-[20px] mr-12 sticky sm:mt-[65px] mt-[300px] font-semibold text-black`}
                 >
                   <div>{rows[dataIndex][1]}</div>
                   <div>{convertNumberToLang(rows[dataIndex][4], "ar")}م</div>
